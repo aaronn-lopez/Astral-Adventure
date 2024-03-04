0abe392737a4c63c338357eca411aabcd18791eb
Joodoon <jooodoon@gmail.com>
2024-03-03 18:16:41 -0800

    removed redundant code

--- src/main/java/Processing.java
+++ src/main/java/Processing.java
@@ -31,10 +31,6 @@ public class Processing extends PApplet {
         Gameobject.init(this);
         gameManager = new GameManager();
 
-        // Calculate the starting position for the grid to be centered
-        int startX = (width - numCols * tileSize) / 2;
-        int startY = (height - numRows * tileSize) / 2;
-
         gameManager.instantiate(Objects.Player, 0, 0);
         gameManager.instantiate(Objects.Battery, 4, 4);
         gameManager.instantiate(Objects.Blackhole, 7, 7);
@@ -49,28 +45,6 @@ public class Processing extends PApplet {
         gameManager.instantiate(Objects.Blackhole, 7, 5);
 
         bg = new Gameobject(new Transform(640, 360, 0, 1), "src/main/Sprites/Space Background.png");
-
-        // There are three sprites per 'ground' tile so that we get the cool 'layering' effect
-        cliffSprites = new Gameobject[numRows][numCols];
-        outlineSprites = new Gameobject[numRows][numCols];
-        pathSprites = new Gameobject[numRows][numCols];
-
-        String pathSprite = "src/main/Sprites/Path.png";
-        String outlineSprite = "src/main/Sprites/Path Outline.png";
-        String cliffSprite = "src/main/Sprites/Path Cliff.png";
-
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                int x = startX + j * tileSize + tileSize / 2;
-                int y = startY + i * tileSize + tileSize / 2;
-
-                Transform transform = new Transform(x, y, 0, 1);
-
-                cliffSprites[i][j] = new Gameobject(transform, cliffSprite);
-                outlineSprites[i][j] = new Gameobject(transform, outlineSprite);
-                pathSprites[i][j] = new Gameobject(transform, pathSprite);
-            }
-        }
     }
 
     // Called once every frame
@@ -78,7 +52,6 @@ public class Processing extends PApplet {
     public void draw(){
         bg.draw();
 
-        // Need to draw each component separately to keep proper layer order
         for(int pass = 0; pass < 3; pass++) {
             for (int i = 0; i < numRows; i++) {
                 for (int j = 0; j < numCols; j++) {

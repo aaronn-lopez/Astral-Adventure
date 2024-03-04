de3a88a199d1d655864dbcdc3eca3c4fd1733636
ala233 <ala233@sfu.ca>
2024-03-04 15:41:10 -0800

    added the first instance of player key inputs to move the character.

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,7 +1,36 @@
+import processing.core.PConstants;
+
+import static processing.core.PApplet.sin;
+
 public abstract class Character extends Gameobject{
+
     Character(Transform transform, String spritePath){
         super(transform, spritePath);
     }
 
-    void move(){};
+    void move(){
+    };
+
+    // First implementation of the character moving when user presses w,s,a,d
+    public void draw(){
+        if(p.keyPressed){
+            if(p.key == 'w' || p.key == 'W')
+            {
+                this.Transform.y -= 1;
+            }
+            else if(p.key == 'a' || p.key == 'A')
+            {
+                this.Transform.x -= 1;
+            }
+            else if(p.key == 's' || p.key == 'S')
+            {
+                this.Transform.y += 1;
+            }
+            else if(p.key == 'd' || p.key == 'D') {
+                this.Transform.x += 1;
+            }
+        }
+        p.imageMode(PConstants.CENTER);
+        p.image(this.Sprite, this.Transform.x, this.Transform.y);
+    }
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,4 +2,5 @@ public class Player extends Character{
     Player(Transform transform){
         super(transform, "src/main/Sprites/Astronaut.png");
     }
+
 }

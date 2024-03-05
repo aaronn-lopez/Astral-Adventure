23506218e5f8f4af2bd1a87acb98374ad1595142
ala233 <ala233@sfu.ca>
2024-03-04 18:20:02 -0800

    moved the code to move character into player class as only the player should be controlled by the user.

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -8,29 +8,5 @@ public abstract class Character extends Gameobject{
         super(transform, spritePath);
     }
 
-    void move(){
-    };
-
-    // First implementation of the character moving when user presses w,s,a,d
-    public void draw(){
-        if(p.keyPressed){
-            if(p.key == 'w' || p.key == 'W')
-            {
-                this.Transform.y -= 1;
-            }
-            else if(p.key == 'a' || p.key == 'A')
-            {
-                this.Transform.x -= 1;
-            }
-            else if(p.key == 's' || p.key == 'S')
-            {
-                this.Transform.y += 1;
-            }
-            else if(p.key == 'd' || p.key == 'D') {
-                this.Transform.x += 1;
-            }
-        }
-        p.imageMode(PConstants.CENTER);
-        p.image(this.Sprite, this.Transform.x, this.Transform.y);
-    }
+    void move(){};
 }
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,35 @@
+import processing.core.PConstants;
+
 public class Player extends Character{
     Player(Transform transform){
         super(transform, "src/main/Sprites/Astronaut.png");
+    }
+
+    void move()
+    {
+        if(p.keyPressed){
+            if(p.key == 'w' || p.key == 'W')
+            {
+                this.Transform.y -= 1;
+            }
+            else if(p.key == 'a' || p.key == 'A')
+            {
+                this.Transform.x -= 1;
+            }
+            else if(p.key == 's' || p.key == 'S')
+            {
+                this.Transform.y += 1;
+            }
+            else if(p.key == 'd' || p.key == 'D') {
+                this.Transform.x += 1;
+            }
+        }
+    }
+
+    public void draw(){
+        move();
+        p.imageMode(PConstants.CENTER);
+        p.image(this.Sprite, this.Transform.x, this.Transform.y);
     }
 
 }

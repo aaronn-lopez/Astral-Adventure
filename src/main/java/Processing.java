b0b882ad15269e0189122105600a3c2758475232
Joodoon <jsm36@sfu.ca>
2024-03-14 21:42:23 -0700

    Added basic start screen

--- src/main/java/Processing.java
+++ src/main/java/Processing.java
@@ -85,6 +85,7 @@ public class Processing extends PApplet {
                 guiManager.pause();
                 break;
             case GUIState.Start:
+                guiManager.startScreen();
                 break;
             case GUIState.End:
                 break;

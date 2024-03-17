95a3a71a09e2a2c3981e42d927cce9d512bd5504
Joodoon <jsm36@sfu.ca>
2024-03-17 15:09:27 -0700

    Moved oxygen dissapearance logic to respect class hierarchy and reduce redandant code

--- src/main/java/OxygenTank.java
+++ src/main/java/OxygenTank.java
@@ -7,24 +7,6 @@ public class OxygenTank extends Collectable{
         super(transform, "src/main/Sprites/OxygenTank.png");
     }
 
-    public void draw(){
-        this.Transform.offsetY += (float) (-sin((float) p.frameCount / 10) * 1.1);
-
-        p.fill(0, 0, 0,100);
-        p.noStroke();
-        p.ellipseMode(PConstants.CENTER);
-        p.ellipse(this.Transform.x, this.Transform.y, 48, 32);
-
-        if(GameManager.gameManager.elapsedTime >= GameManager.gameManager.oxygenTankDisappearTime)
-        {
-            GameManager.getCell(this.Transform.gridX, this.Transform.gridY).interactable = null;
-        }
-
-
-        p.imageMode(PConstants.CENTER);
-        p.image(this.Sprite, this.Transform.x + this.Transform.offsetX, this.Transform.y + this.Transform.offsetY);
-    }
-
     @Override
     public void collect() {
         //if your oxygen is above 75% then there will be "overflow"

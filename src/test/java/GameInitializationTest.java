896b037500602995563d1bd6b81dac8ac690066f
Brandon Quon <bqa7@sfu.ca>
2024-03-23 17:45:08 -0700

    I ran a successful test on checking if the gameManager and guiManager loaded

--- src/test/java/GameInitializationTest.java
+++ src/test/java/GameInitializationTest.java
@@ -5,6 +5,7 @@ public class GameInitializationTest {
 
     @Test
     public void testGameInitialization() {
+        // Test logic
         Processing game = new Processing();
         assertNotNull(game.gameManager);
         assertNotNull(game.guiManager);

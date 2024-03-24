62c995cbfd5fc12b8f800847e84329f5926230bd
Brandon Quon <bqa7@sfu.ca>
2024-03-23 18:58:36 -0700

    Implemented testing map for loading the gridX and gridY and just overall loading

--- src/test/java/MapLoaderTest.java
+++ src/test/java/MapLoaderTest.java
@@ -1,16 +1,38 @@
+import org.junit.Before;
 import org.junit.Test;
+import java.io.File;
+import java.util.Scanner;
 import static org.junit.Assert.*;
 
 public class MapLoaderTest {
+    private Map map;
+    private String testMapPath = "src/test/resources/TestLevels/TestLevel1.txt";
+
+    @Before
+    public void setUp() {
+        // Initialize the Map object
+        map = new Map();
+    }
 
     @Test
     public void testMapLoading() {
-        Map map = new Map();
+        // Load the test map file
+        loadTestMap();
 
-        String testMapPath = "src/test/resources/TestLevels/TestLevel1.txt";
-        map.newMap(testMapPath);
-
+        // Assert that gridX and gridY are greater than 0
         assertTrue("gridX should be greater than 0", map.gridX > 0);
         assertTrue("gridY should be greater than 0", map.gridY > 0);
+    }
+
+    private void loadTestMap() {
+        try {
+            File mapFile = new File(testMapPath);
+            Scanner fileScanner = new Scanner(mapFile);
+            map.gridX = fileScanner.nextInt();
+            map.gridY = fileScanner.nextInt();
+            fileScanner.close();
+        } catch (Exception e) {
+            fail("Failed to load test map: " + e.getMessage());
+        }
     }
 }

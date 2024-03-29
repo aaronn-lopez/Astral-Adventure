bc3b4a650388605d9b32fe133bb9cde6d815e22a
Brandon Quon <bqa7@sfu.ca>
2024-03-28 18:57:17 -0700

    Fixed code testing correct number of instantiated walking and hiding aliens

--- src/test/java/TestInstantiateObjects.java
+++ src/test/java/TestInstantiateObjects.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+
 public class TestInstantiateObjects {
     private GameManager gameManager;
 
@@ -10,17 +12,25 @@ public class TestInstantiateObjects {
         map.newMap("src/test/resources/TestLevels/dummyMap.txt");
         gameManager = GameManager.gameManager;
     }
-
+    @Test
+    public void testInstantiatePlayer() {
+        // Check if player is instantiated
+        Gameobject player = gameManager.player;
+        assertNotNull("Player should be instantiated", player);
+    }
     @Test
     public void testInstantiateWalkingAlien() {
-        Map map = new Map();
-        map.newMap("src/test/resources/TestLevels/dummyMap.txt");
-
         // Check the number of enemies present on the map
-        int expectedNumEnemies = 1; // Assuming only one walking alien is present on the map
+        int expectedNumWalkingAliens = 1; // Assuming only one walking alien is present on the map
+        int actualNumWalkingAliens = 0;
+        for(int i = 0; i < gameManager.enemies.size(); i++){
+            if(gameManager.enemies.get(i) instanceof Spike){
+                actualNumWalkingAliens++;
+            }
+        }
         int actualNumEnemies = gameManager.enemies.size();
 
-        assertEquals("Number of walking aliens should match", expectedNumEnemies, actualNumEnemies);
+        assertEquals("Number of walking aliens should match", expectedNumWalkingAliens, actualNumWalkingAliens);
     }
 
     @Test
@@ -29,9 +39,22 @@ public class TestInstantiateObjects {
         map.newMap("src/test/resources/TestLevels/dummyMapWithZeroWalkingAliens.txt");
 
         // Check the number of enemies present on the map
-        int expectedNumEnemies = 0; // Assuming zero walking aliens are present on the map
+        int expectedNumEnemies = 0;
         int actualNumEnemies = gameManager.enemies.size();
 
         assertEquals("Number of walking aliens should match", expectedNumEnemies, actualNumEnemies);
+    }
+
+    @Test
+    public void testInstantiateHidingAlien() {
+        // Check if hiding alien is instantiated
+        int expectedNumHidingAliens = 1;
+        int actualNumHidingAliens = 0;
+        for(int i = 0; i < gameManager.enemies.size(); i++){
+            if(gameManager.enemies.get(i) instanceof Spike){
+                actualNumHidingAliens++;
+            }
+        }
+        assertEquals("Number of hiding aliens should match", expectedNumHidingAliens, actualNumHidingAliens);
     }
 }

8b403994e5e4e946db931a062cf3bfb126c388dc
Brandon Quon <bqa7@sfu.ca>
2024-03-29 20:43:23 -0700

    Working on testing proper win and loss outcome testing

---
+++ src/test/java/GameOutcomeTests.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class GameOutcomeTests {
+
+    private GameManager gameManager;
+
+    @Before
+    public void setUp() {
+
+    }
+}

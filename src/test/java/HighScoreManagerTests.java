91989193aa9999c655393a466d1c2070f4de62d9
Brandon Quon <bqa7@sfu.ca>
2024-04-02 19:02:22 -0700

    Wrote the setup for the scoreboard testing

--- src/test/java/HighScoreManagerTests.java
+++ src/test/java/HighScoreManagerTests.java
@@ -1,2 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class HighScoreManagerTests {
+
+    private Scoreboard scoreboard;
+
+    @Before
+    public void setUp() {
+        scoreboard = new Scoreboard();
+    }
 }

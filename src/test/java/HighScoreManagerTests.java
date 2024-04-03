593a97bf1264a578fb96620df3ffbe60dca97f55
Brandon Quon <bqa7@sfu.ca>
2024-04-02 21:03:40 -0700

    Okay new approach because my approach doesnt work after an hour+

--- src/test/java/HighScoreManagerTests.java
+++ src/test/java/HighScoreManagerTests.java
@@ -2,38 +2,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
 public class HighScoreManagerTests {
 
     private Scoreboard scoreboard;
-    private static final String DUMMY_SCORES_FILE_PATH = "src/test/resources/dummyScores.txt";
+    private static final String DUMMY_SCORES_FILE_PATH = "src/test/resources/level1_scores.txt";
 
     @Before
     public void setUp() {
         scoreboard = new Scoreboard();
-        loadDummyScoresFromFile();
     }
 
-    private void loadDummyScoresFromFile() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(DUMMY_SCORES_FILE_PATH));
-            String line;
-            int level = 1;
-            // Read scores from the dummy file and update the scoreboard
-            while ((line = reader.readLine()) != null) {
-                scoreboard.updateScoreboard(Integer.parseInt(line), level);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-//    @Test
-//    public void addScore() {
-//        scoreboard.updateScoreboard(100, 5);
-//        assertEquals("100\n", scoreboard.getScores(5));
-//    }
 }
